0e956f58d12b3394f5991353fcde3a3d2c009b81
gabriel <akog@mymacewan.ca>
2024-03-12 21:43:18 -0600

    Add project files.

---
+++ AssignmentOne/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Create three students
+using AssignmentOne.EnumFolder;
+using AssignmentOne;
+
+var newStudent = new Student();
+
+var student1 = newStudent.CreateStudent(001, StudentType.FullTime, "John", "Anderson");
+var student2 = newStudent.CreateStudent(002, StudentType.FullTime, "Karen", "Smith");
+var student3 = newStudent.CreateStudent(003, StudentType.PartTime, "Kevin", "Peters");
+
+// Output header
+Console.WriteLine("Assignment One");
+
+// Output students
+student1.OutputStudent(student1);
+student2.OutputStudent(student2);
+student3.OutputStudent(student3);
---
+++ AssignmentOne/Student.cs
@@ -0,0 +1,42 @@
+﻿using AssignmentOne;
+using AssignmentOne.EnumFolder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentOne
+{
+    internal class Student
+    {
+        public int Id;
+        public StudentType Type;
+        public string FirstName;
+        public string LastName;
+
+
+        public Student CreateStudent(int id, StudentType type, string firstName, string lastName)
+        {
+            if (id == null) id = 0;
+            if (firstName == null) firstName = "Unknown";
+            if (lastName == null) lastName = "";
+
+            var newStudent = new Student();
+
+            newStudent.Id = id;
+            newStudent.Type = type;
+            newStudent.FirstName = firstName;
+            newStudent.LastName = lastName;
+
+            return newStudent;
+        }
+
+        public void OutputStudent(Student student)
+        {
+            Console.WriteLine($"Student: {student.Id} {student.FirstName} {student.LastName} ({student.Type})");
+        }
+    }
+
+}
+    
